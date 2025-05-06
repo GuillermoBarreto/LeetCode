2e18a1cf3cfbe011a8f34886c2d3edbd72e506fd
GuillermoBarreto <Guilleelias25@gmail.com>
2025-05-06 09:33:54 -0400

    addtwonumber

--- TwoSum.py
+++ TwoSum.py
@@ -34,4 +34,10 @@
 # -109 <= target <= 109
 # Only one valid answer exists.
 
-class Solutiion
+def twoSum(nums, target):
+    num_map = {}  # To store number and its index
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
