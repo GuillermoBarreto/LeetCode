c2da3ae303cc12af12746ce546f687f767d709d8
GuillermoBarreto <Guilleelias25@gmail.com>
2025-05-11 13:13:19 -0400

    creating the loop and subtract if current value is less than previous value

--- RomantoInteger.py
+++ RomantoInteger.py
@@ -43,5 +43,14 @@
 # It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
 class solution:
-    def 
+    def romanToInt(self, s: str) -> int: 
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 
+            'D': 500, 'M': 1000
+        }
+
+        total = 0 
+        prev_value = 0
+        
     
