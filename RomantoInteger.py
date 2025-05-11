323e824143d9a90163f28bf58d12466d21a79814
GuillermoBarreto <Guilleelias25@gmail.com>
2025-05-11 13:06:13 -0400

    We loop from right to left and keep a running total.

--- RomantoInteger.py
+++ RomantoInteger.py
@@ -42,5 +42,6 @@
 # s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 # It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
-
+class solution:
+    def 
     
